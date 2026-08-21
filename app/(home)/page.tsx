8cb9eb98d78d1164486d9efde5da93f33d1dfee0
Jack Carter <thecarpetjasp@gmail.com>
2026-08-21 11:13:47 +0100

    feat: added Live Oak at a Glance section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import About from "./(about)/About";
 import Contact from "./(contact)/Contact";
 import EngProcess from "./(eng-process)/EngProcess";
 import Hero from "./(hero)/Hero";
+import LiveOakAtAGlance from "./(live-oak-at-a-glance)/LiveOakAtAGlance";
 import Location from "./(location)/Location";
 import News from "./(news)/News";
 import Overview from "./(overview)/Overview";
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <main className="relative z-10 bg-background">
       <Hero></Hero>
+      <LiveOakAtAGlance></LiveOakAtAGlance>
       <Overview></Overview>
       <About></About>
       <EngProcess></EngProcess>
